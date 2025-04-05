8b3f61bc3e94b3b194668514f7b546a185f9fbb1
Eduardo de Oliveira <109693585+eduoliveira1504@users.noreply.github.com>
2025-04-04 21:33:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 
-symbols = ["CCOEY", "TCEHY", "UBSFY", "RBLX", "EA"]
+symbols = ["NTDOY", "TCEHY", "EA", "RBLX", "UBSFY"]
 
 def get_stock_data(symbol, period):
     stock = yq.Ticker(symbol)
